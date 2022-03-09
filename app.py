271aad63f53f42b0bcca9e279ac8fe1067407393
github <github>
2022-03-09 19:05:37 +0900

    dd

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ import pandas as pd
 import plotly.express as px  # (version 4.7.0)
 
 df4 = px.data.gapminder()
-st.write(df4.head())
+#st.write(df4.head())
+
+fig = px.scatter(df4, x="gdpPercap", y="lifeExp", size="pop",color="continent",hover_name="country",animation_frame="year")
+fig.show()
