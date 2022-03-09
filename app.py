8763689e0201ae3b6858b61f94eb9b6ffae291d2
github <github>
2022-03-09 19:34:05 +0900

    ss

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-import streamlit as st
+import streamlit as st 
 import pandas as pd
-import plotly.express as px  # (version 4.7.0)
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from matplotlib.backends.backend_agg import RendererAgg
 
-df4 = px.data.gapminder()
+#gdp グラフ
+#df4 = px.data.gapminder()
 #st.write(df4.head())
+#fig = px.scatter(df4, x="gdpPercap", y="lifeExp", size="pop",color="continent",hover_name="country",animation_frame="year")
+#st.write(fig)
+#url = 'https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv'
+#df = pd.read_csv(url)
+#st.write(df)
 
-fig = px.scatter(df4, x="gdpPercap", y="lifeExp", size="pop",color="continent",hover_name="country",animation_frame="year")
-st.write(fig)
-
-url = 'https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv'
-df = pd.read_csv(url)
-st.write(df)
+#configuration of the page
+st.set_page_config(layout="wide")
+#load dataframes
+url = 'https://github.com/max-lutz/national-assembly/blob/main/data/df_dep.csv'
+url2 = 'https://github.com/max-lutz/national-assembly/blob/main/data/df_polpar.csv'
+df_dep = pd.read_csv(url)
+df_pol_par = pd.read_csv(url2)
+st.title('French national assembly vizualisation tool')
+st.markdown("""
+This app performs simple visualization from the open data from the french national assembly!
+""")
+st.write(df_dep)
+st.write(df_pol_par)
