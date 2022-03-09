ae9bfe0e12599e86a132ae838678e50c0e9d7cad
github <github>
2022-03-10 07:56:14 +0900

    ass

--- app.py
+++ app.py
@@ -51,3 +51,11 @@ st.write(df4.head())
 st.title('GAP')
 fig4 = px.scatter(df4, x="gdpPercap", y="lifeExp", size="pop",color="continent",hover_name="country",animation_frame="year")
 fig4
+
+df5 = pd.read_csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/6_SeveralNum.csv")
+st.write(df5.head())
+
+fig5 = px.scatter_matrix(df5, 
+                        dimensions=["mpg", "disp", "drat", "hp", "qsec", "wt"], 
+                        color="gear")
+fig5
