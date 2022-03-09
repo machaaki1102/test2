7d7eac5cf33b944503ff91131f9f3b7202d4add9
github <github>
2022-03-10 08:56:04 +0900

    zs

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ fig7
 
 #mapbox
 px.set_mapbox_access_token('pk.eyJ1IjoibWFjaGFha2kiLCJhIjoiY2wwamVyanUxMGJ2bTNqcjU4dGZtdWdoZyJ9.Vk57Qp-OPGYFkGdgTB6iYw')
-df9 = pd.read_csv('covid19.csv',header=1)
+df9 = pd.read_csv('covid19.csv')
 fig9 = px.scatter_mapbox(df9,lat="lat", lon="lon",size="pop",color="pop",size_max=80,zoom=3, height=500)
 fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig9
