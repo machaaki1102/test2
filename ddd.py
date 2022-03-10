0d41d5b56dcdfcefd4beca501736b40f2036dc86
github <github>
2022-03-10 20:45:09 +0900

    ss

--- ddd.py
+++ ddd.py
@@ -6,12 +6,14 @@ import PIL.ExifTags as Exiftags
 
 
 st.title('画像から緯度・経度取得')
-img = st.file_uploader('写真アップロード',type='jpg')
-if img is True:
-    exif = img._getexif()
-#sデータの一覧
-    for id,value in exif.items():
-        st.write(id,value)
+#img = st.file_uploader('写真アップロード',type='jpg')
+im = Image.open('DSC_0106.JPG')
+im
+#if img is True:
+#    exif = img._getexif()
+##sデータの一覧
+#    for id,value in exif.items():
+#        st.write(id,value)
 #
 def get_gps(frame):
 #画像ファイルを開く
