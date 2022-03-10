d6df5d8be023f548768e8739b836e97f7449ff3a
github <github>
2022-03-10 09:34:00 +0900

    sｓ

--- lon.py
+++ lon.py
@@ -3,8 +3,9 @@ import pandas as pd
 from pillow import Image
 import numpy as np
 
+st.titile('画像から緯度・経度取得')
 img = st.file_uploader('写真アップロード',type='jpg')
 exif = img_getexif()
+
 for id,value in exif.items():
     st.write(id,value)
-    
