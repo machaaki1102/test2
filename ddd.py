86a958166cac3cdf735cb2a42e91cb042d3b860b
github <github>
2022-03-10 21:05:53 +0900

    ss

--- ddd.py
+++ ddd.py
@@ -4,36 +4,56 @@ from PIL import Image
 import numpy as np
 import PIL.ExifTags as Exiftags
 
+#写真から緯度経度をとる。
+def get_gps(fname):
+    # 画像ファイルを開く --- (*1)
+    im = Image.open(fname)
+    # EXIF情報を辞書型で得る
+    exif = {
+        ExifTags.TAGS[k]: v
+        for k, v in im._getexif().items()
+        if k in ExifTags.TAGS
+    }
+    # GPS情報を得る --- (*2)
+    gps_tags = exif["GPSInfo"]
+    gps = {
+        ExifTags.GPSTAGS.get(t, t): gps_tags[t]
+        for t in gps_tags
+    }
+    # 緯度経度情報を得る --- (*3)
+    def conv_deg(v):
+        # 分数を度に変換
+        d = float(v[0][0]) / float(v[0][1])
+        m = float(v[1][0]) / float(v[1][1])
+        s = float(v[2][0]) / float(v[2][1])
+        return d + (m / 60.0) + (s / 3600.0)
+    lat = conv_deg(gps["GPSLatitude"])
+    lat_ref = gps["GPSLatitudeRef"]
+    if lat_ref != "N": lat = 0 - lat
+    lon = conv_deg(gps["GPSLongitude"])
+    lon_ref = gps["GPSLongitudeRef"]
+    if lon_ref != "E": lon = 0 - lon
+    return lat, lon
 
+
+#body
 st.title('画像から緯度・経度取得')
 #img = st.file_uploader('写真アップロード',type='jpg')
 img = Image.open('DSC_0106.JPG')
+lat, lon = get_gps()
+st.write(f'経度:{lat}緯度:{lon}')
+#写真表示
 #st.image(im)
+
 #EXIF情報をとる。
-exif = img._getexif()
-for id,value in exif.items():
-    st.write(id,value)
-
-
+#exif = img._getexif()
+#for id,value in exif.items():
+#    st.write(id,value)
 #if img is True:
 #    exif = img._getexif()
 ##sデータの一覧
 #    for id,value in exif.items():
 #        st.write(id,value)
-#
-def get_gps(frame):
-#画像ファイルを開く
-    im = Image.oepn(frame)
-    exif = {
-        Exiftags.TAGS[k]:v
-        for k ,v in im_getexif().items()
-            if k in Exiftags.TAGS
-    }
-#GPS情報を得る
-    gps_tags = exif['GPSinfo']
-    gps = {
-        ExifTags.GPSTAGS.get(t,t):gps_tags[t]
-        for i in gps_tags
-    }
- #緯度経度情報を得る
- 
+
+
+lat, lon = get_gps()
