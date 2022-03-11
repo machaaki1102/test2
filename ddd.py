4dffc7b2ecfcbaf09ebad79858461e3d86146158
github <github>
2022-03-11 17:47:39 +0900

    ss

--- ddd.py
+++ ddd.py
@@ -22,18 +22,31 @@ def get_gps(fname):
     }
     # 緯度経度情報を得る --- (*3)
     # 分数を度に変換
-    def conv_deg(v):
-        d = float(v[0][0]) / float(v[0][1])
-        m = float(v[1][0]) / float(v[1][1])
-        s = float(v[2][0]) / float(v[2][1])
-        return d + (m / 60.0) + (s / 3600.0)
-    lat = conv_deg(gps["GPSLatitude"])
+#    def conv_deg(v):
+#        d = float(v[0][0]) / float(v[0][1])
+#        m = float(v[1][0]) / float(v[1][1])
+#        s = float(v[2][0]) / float(v[2][1])
+#        return d + (m / 60.0) + (s / 3600.0)
+    lat = gps["GPSLatitude"]
     lat_ref = gps["GPSLatitudeRef"]
     if lat_ref != "N": lat = 0 - lat
     lon = conv_deg(gps["GPSLongitude"])
     lon_ref = gps["GPSLongitudeRef"]
     if lon_ref != "E": lon = 0 - lon
     return lat, lon
+
+#    def conv_deg(v):
+#        d = float(v[0][0]) / float(v[0][1])
+#        m = float(v[1][0]) / float(v[1][1])
+#        s = float(v[2][0]) / float(v[2][1])
+#        return d + (m / 60.0) + (s / 3600.0)
+#    lat = conv_deg(gps["GPSLatitude"])
+#    lat_ref = gps["GPSLatitudeRef"]
+#    if lat_ref != "N": lat = 0 - lat
+#    lon = conv_deg(gps["GPSLongitude"])
+#    lon_ref = gps["GPSLongitudeRef"]
+#    if lon_ref != "E": lon = 0 - lon
+#    return lat, lon
 
 #
 
@@ -52,7 +65,9 @@ gps = {
   for t in gps_tags
 }
 
-st.write(gps)
+#st.write(gps)
+st.wite(gps["GPSLatitude"])
+
 #body
 st.title('画像から緯度・経度取得')
 #img = st.file_uploader('写真アップロード',type='jpg')
