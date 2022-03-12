aaa02dcd9c9ed1da8414f464ef756c62992dcc1d
github <github>
2022-03-12 10:52:31 +0900

    ss

--- ddd.py
+++ ddd.py
@@ -35,11 +35,12 @@ lat,lon = chape(img)
 st.write(f'経度:{lat}緯度:{lon}')
 
 df9 = pd.DataFrame(np.array((lat,lon)).reshape(1,2),columns=['lat','lon'])
+st.write(df9)
 px.set_mapbox_access_token('pk.eyJ1IjoibWFjaGFha2kiLCJhIjoiY2wwamVyanUxMGJ2bTNqcjU4dGZtdWdoZyJ9.Vk57Qp-OPGYFkGdgTB6iYw')
 #df9 = pd.read_csv('covid19.csv')
-fig9 = px.scatter_mapbox(df9,lat="lat", lon="lon",size="pop",color="pop",size_max=80,zoom=3, height=500)
+#fig9 = px.scatter_mapbox(df9,lat="lat", lon="lon",size="pop",color="pop",size_max=80,zoom=3, height=500)
 #fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig9
+#fig9
 
 #st.write(f'経度:{lat}緯度:{lon}')
 #写真表示
