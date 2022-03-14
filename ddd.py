7e11cdccd0d42647f762ee19398ac7bdd426c0d2
github <github>
2022-03-15 07:15:07 +0900

    ss

--- ddd.py
+++ ddd.py
@@ -31,8 +31,8 @@ def chape(img):
 
 #body
 st.title('画像から緯度・経度取得')
-img = Image.open('IMG_1010.JPG')
-#img = st.file_uploader('写真アップロード',type='jpg')
+#img = Image.open('IMG_1010.JPG')
+img = st.file_uploader('写真アップロード',type='jpg')
 lat,lon = chape(img)
 st.write(f'経度:{lat}緯度:{lon}')
 
