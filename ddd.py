149b43c1a27ab22649398f4d7c2c5e03c40c30cd
github <github>
2022-03-16 08:36:28 +0900

    ss

--- ddd.py
+++ ddd.py
@@ -58,6 +58,7 @@ st.title('画像から緯度・経度取得')
 #img = Image.open('IMG_1010.JPG')
 img = st.file_uploader('写真アップロード',type='jpg')
 st.image(img)
+st.write(img._getexif().items())
 
 exif = {
   ExifTags.TAGS[k]: v
