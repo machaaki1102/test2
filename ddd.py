334263ca9c13a6823938892041164bc6d7882602
github <github>
2022-03-16 22:06:03 +0900

    ss

--- ddd.py
+++ ddd.py
@@ -7,11 +7,9 @@ import numpy as np
 import plotly.express as px
 
 
-#img　に入った画像の経度緯度を取る。
-
+#img　に入った画像の経度緯度を取る。ss
 #parts
 def chape(img):
-#  img = Image.open('IMG_1010.JPG')
   exif = {
     ExifTags.TAGS[k]: v
     for k, v in img._getexif().items()
@@ -30,30 +28,6 @@ def chape(img):
   return lat,lon
 
 #body
-#st.title('画像から緯度・経度取得')
-#img = Image.open('IMG_1010.JPG')
-#img = st.file_uploader('写真アップロード',type='jpg')
-#lat,lon = chape(img)
-#st.write(f'経度:{lat}緯度:{lon}')
-
-#マップングする。基
-#df9 = pd.DataFrame(np.array((lat,lon)).reshape(1,2),columns=['lat','lon'])
-#st.write(df9)
-#px.set_mapbox_access_token('pk.eyJ1IjoibWFjaGFha2kiLCJhIjoiY2wwamVyanUxMGJ2bTNqcjU4dGZtdWdoZyJ9.Vk57Qp-OPGYFkGdgTB6iYw')
-#df9 = pd.read_csv('covid19.csv')
-#fig9 = px.scatter_mapbox(
-#  data_frame=df9,
-#  lat="lat",
-#  lon="lon",
-#  size="lat",
-#  size_max=10,
-#  zoom=5,
-#  height=500)
-#fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-#fig9 = fig9.update_layout(mapbox_style='open-street-map')
-#fig9
-
-#body志向
 st.title('画像から緯度・経度取得')
 #img = Image.open('IMG_1010.JPG')
 img = st.file_uploader('写真アップロード',type='jpg')
@@ -79,21 +53,3 @@ if img:
   fig9.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
   fig9 = fig9.update_layout(mapbox_style='open-street-map')
   fig9
-
-
-
-
-#st.write(f'経度:{lat}緯度:{lon}')
-#写真表示
-#st.image(im)
-
-
-#EXIF情報をとる。
-#exif = img._getexif()
-#for id,value in exif.items():
-#    st.write(id,value)
-#if img is True:
-#    exif = img._getexif()
-##sデータの一覧
-#    for id,value in exif.items():
-#        st.write(id,value)
