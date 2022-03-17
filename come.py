003215e4e632aecf36ca6f6165f450cc03181670
github <github>
2022-03-18 07:03:22 +0900

    ss

---
+++ come.py
@@ -0,0 +1,11 @@
+#chage = st.selectbox("select fanction",["緯度経度","米カウント"])
+#if 'page' not in st.session_state:
+#st.session_state.page = 1
+
+#ステージ入れ替え
+#if chage == "緯度経度":
+#  st.session_state.page = 1
+#  st.experimental_rerun()
+#if chage == "米カウント":
+#  st.session_state.page = 2
+# st.experimental_rerun()
--- ddd.py
+++ ddd.py
@@ -28,19 +28,6 @@ def chape(img):
 
 
 #body
-chage = st.selectbox("select fanction",["緯度経度","米カウント"])
-
-if 'page' not in st.session_state:
-  st.session_state.page = 1
-
-#ステージ入れ替え
-if chage == "緯度経度":
-  st.session_state.page = 1
-#  st.experimental_rerun()
-if chage == "米カウント":
-  st.session_state.page = 2
-  st.experimental_rerun()
-
 if st.session_state.page == 1:
   st.title('画像から緯度・経度取得')
   #img = Image.open('IMG_1010.JPG')
