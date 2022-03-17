c72b7d46824c80226bc38bd07b626f97764efb57
github <github>
2022-03-18 07:23:14 +0900

    sｓ

--- ddd.py
+++ ddd.py
@@ -29,7 +29,7 @@ def chape(img):
 
 #body
 st.header('画像から緯度・経度取得')
-st.write('※exif情報(位置情報がないものはエラーになります。')
+#st.write('※exif情報(位置情報がないものはエラーになります。')
 #img = Image.open('IMG_1010.JPG')
 img = st.file_uploader('写真から緯度経度を取得出来、地図上で表します。',type='jpg')
 #img = st.camera_input('Take a picure')
