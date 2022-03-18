64c786ee3b66f8070e968f679ecb178d59fc73bd
github <github>
2022-03-18 12:55:07 +0900

    ss

--- gazo.py
+++ gazo.py
@@ -32,8 +32,9 @@ st.header('画像から緯度・経度取得')
 #st.write('※exif情報(位置情報がないものはエラーになります。')
 #img = Image.open('IMG_1010.JPG')
 img = st.file_uploader('写真から緯度経度を取得出来、地図上で表します。',type='jpg')
-st.write(img.name)
-st.write(img.getbuffer())
+if img: 
+  with open(img.name, 'wb') as f:
+    f.write(img.getbuffer())
     
 #img = st.camera_input('Take a picure')
 if img is not None:
