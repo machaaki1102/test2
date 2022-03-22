106bfa8d56a26a47612100715ff6246db8da9cba
github <github>
2022-03-22 21:10:17 +0900

    ss

--- sqlite3.py
+++ sqlite3.py
@@ -6,3 +6,12 @@ import seaborn as sns
 import os
 import sqlite3
 
+con = sqlite3.connect('example.db')
+cur = con.cursor()
+
+#Create table
+cur.execute('''CREATE TABLE stocks
+                (date text,trans text,symbol text,qty real, price real)''')
+
+#Insert a row of data
+cur.excute("INSERT INTO stocks VALUES('2006-01-05','BUY','RHAT','100.35.14')")
