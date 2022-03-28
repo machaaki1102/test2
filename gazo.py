0e7cc1a463dca99630b9b9e1f457a9d450543dfc
github <github>
2022-03-28 21:03:38 +0900

    ss

--- gazo.py
+++ gazo.py
@@ -23,7 +23,7 @@ def chape(img): #imgは、JPEGそのまま入れた。
   }
   lat = float(gps["GPSLatitude"][0])+float(gps["GPSLatitude"][1]/60)+float(gps["GPSLatitude"][2]/3600)
   lon = float(gps["GPSLongitude"][0])+float(gps["GPSLongitude"][1]/60)+float(gps["GPSLongitude"][2]/3600)
-  date = exif["DateTimeOriginal"]
+  date = gps["DateTimeOriginal"]
 #  lat = float(gps["GPSLatitude"][0])+float(gps["GPSLatitude"][1]/100)
 #  lon = float(gps["GPSLongitude"][0])+float(gps["GPSLongitude"][1]/100)
   return lat,lon
