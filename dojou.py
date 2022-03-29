6a1e17811c6ba746df39a91d02f54ae4774f7bf9
github <github>
2022-03-29 14:42:36 +0900

    sss

--- dojou.py
+++ dojou.py
@@ -1,4 +1,17 @@
 from selenium import webdriver
-from time import sleep
+import time 
+import os
 
-browzer = webdriver.chrome('chromedriver.exe')
+browzer = webdriver.Chrome(executable_path='chromedriver.exe')
+
+lat = 34.999418
+lon= 134.990197
+url =f'https://soil-inventory.rad.naro.go.jp/figure.html?lat={lat}&lng={lon}&zoom=15'
+#url2 = 'https://soil-inventory.rad.naro.go.jp/figure.html?lat=34.999418&lng=134.990197&zoom=15'
+browzer.get(url)
+time.sleep(5)
+a = borwzer.find_element_by_class('leaflet-container leaflet-touch leaflet-retina leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom')
+#.click()
+print(a.is_enabled())
+#browzer.quit()
+
