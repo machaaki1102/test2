68ae6178ed89397032d3f51d5dceefe35b4cd359
github <github>
2022-04-01 09:27:28 +0900

    ss

--- gazo.py
+++ gazo.py
@@ -40,7 +40,7 @@ img = st.file_uploader('写真から緯度経度を取得出来、地図上で�
 
 if img is not None:
   img  = Image.open(img)
-  lat,lon,date = chape(img)
+  lat,lon = chape(img)
   st.write(f'経度:{"{:.4f}".format(lat)}緯度:{"{:.4f}".format(lon)}')
   #st.write(date)
 
