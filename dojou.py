2b19b94dd3b57a0a76cc5c5029d403c322f946a5
github <github>
2022-04-07 20:15:50 +0900

    aa

--- dojou.py
+++ dojou.py
@@ -4,40 +4,67 @@ import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
-borwer = webdriver.Chrome(executable_path='chromedriver.exe')
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
 #tools
 def click_by_position(driver, x, y) -> None:
     actions = ActionChains(driver)
 
     # MOVE TO TOP_LEFT (`move_to_element` will guide you to the CENTER of the element)
-    whole_page = driver.find_element_by_tag_name("html")
-    #actions.move_to_element_with_offset(whole_page, 0, 0)
+    whole_page = driver.find_element_by_id("map")
+    actions.move_to_element_with_offset(whole_page, 0, 0)
 
-    actions.move_to_element(whole_page)
     # MOVE TO DESIRED POSITION THEN CLICK
     actions.move_by_offset(x, y)
     actions.click()
 
     actions.perform()
+    print(whole_page.location)
 
 #body
 lat = 34.999418
 lon= 134.990197
 url =f'https://soil-inventory.rad.naro.go.jp/figure.html?lat={lat}&lng={lon}&zoom=15'
 #url2 = 'https://soil-inventory.rad.naro.go.jp/figure.html?lat=34.999418&lng=134.990197&zoom=15'
-borwer.get(url)
-time.sleep(3)
+driver.get(url)
+time.sleep(2)
 
-x = 100
-y = 100
+driver.execute_script("window.scrollTo(0, 200)")
+
+x = 5
+y = 85
+click_by_position(driver, x, y)
+#a = borwer.find_element_by_class_name('leaflet-pane leaflet-marker-pane')
+#driver.execute_script("window.scrollTo(0, (document.getElementById('map').scrolltop))")
+
+time.sleep(2)
+popname = driver.find_element_by_id('popName')
+popExplain = driver.find_element_by_id('popExplain')
+print(popname.text)
+print(popExplain.text)
+
+#driver.execute_script('windows.scrollTO(0,200);')
+# ある要素までスクロールする
+#element = driver.find_element_by_id("map")
+#element = driver.find_element_by_id("map")
+#actions = ActionChains(driver)
+#actions.move_to_element(element)
+#actions.perform()
+
+#print(whole_page.location)
+#print(driver.get_window_size())
+#print(element.is_enabled())
+
+#actions.move_to_element(element)
+#actions.perform()
+
+
 #click_by_position(borwer, x, y)
 
-click_by_position(borwer, x, y)
+
 #a = browzer.find_element_by_id("map").click()
 #time.sleep(3)
 
 #pyautogui.click(200,600)
-#print(a.is_enabled())
 #browzer.quit()
 
 
@@ -46,9 +73,9 @@ click_by_position(borwer, x, y)
 #actions = ActionChains(borwer)
 
     # MOVE TO TOP_LEFT (`move_to_element` will guide you to the CENTER of the element)
-whole_page = driver.find_element_by_id("buttonLeftBox")
+#whole_page = driver.find_element_by_id("buttonLeftBox")
 #whole_page = driver.find_element(by=By.ID, value='map')
 #whole_page = driver.find_element(map")
 #actions.move_to_element_with_offset(whole_page, 0, 0)
-actions.move_to_element(whole_page)
-actions.perform()
+#actions.move_to_element(whole_page)
+#actions.perform()
