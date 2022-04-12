99a3ac0ced3e724f69713e4e349978227a08e9e9
github <github>
2022-04-13 07:13:12 +0900

    ss

--- fchrom.py
+++ fchrom.py
@@ -5,6 +5,8 @@ import pandas as pd
 df = pd.read_csv('finished_data.csv',encoding='cp932')
 
 #body
+
+#sidebar
 number_N = st.sidebar.slider('N',0,60)
 puls_number_N = st.sidebar.number_input('N_adjustment')
 number_P = st.sidebar.slider('P',0,60)
@@ -12,6 +14,7 @@ puls_number_P = st.sidebar.number_input('p_adjustment')
 number_K = st.sidebar.slider('K',0,60)
 puls_number_K = st.sidebar.number_input('K_adjustment')
 
+#home
 long = len(df.index)
 st.write(f'ヒット件数:{long}')
 st.dataframe(df)
