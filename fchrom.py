1fbecb836bf5b3bc6325f7db95f9f18da348c0ea
github <github>
2022-04-13 07:10:18 +0900

    ss

--- fchrom.py
+++ fchrom.py
@@ -6,7 +6,7 @@ df = pd.read_csv('finished_data.csv',encoding='cp932')
 
 #body
 number_N = st.sidebar.slider('N',0,60)
-puls_number_N = st.sidebar.numbar.input('N　adjustment')
+puls_number_N = st.sidebar.numbar_input('N　adjustment')
 number_P = st.sidebar.slider('P',0,60)
 #puls_number_P = st.sidebar.input('P　adjustment')
 number_K = st.sidebar.slider('K',0,60)
