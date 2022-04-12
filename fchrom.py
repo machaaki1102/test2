d2d2ae2e6dcdc876386af4e4f057b34f0f3d0ed8
github <github>
2022-04-12 21:44:08 +0900

    ss

--- fchrom.py
+++ fchrom.py
@@ -9,6 +9,8 @@ number_N = st.sidebar.slider('N',0,60)
 number_P = st.sidebar.slider('P',0,60)
 number_K = st.sidebar.slider('K',0,60)
 
+long = len(df.index)
+st.write(f'件数；{long}')
 st.data_frame(df)
 
 #st.number_input('Nの調整',0)
