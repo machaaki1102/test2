11cc8b846fd182bb631eff851e42b839ef3a4c9a
github <github>
2022-04-12 21:42:44 +0900

    ss

--- fchrom.py
+++ fchrom.py
@@ -1,9 +1,14 @@
 import stramlit as st
 import pandas as pd
 
+#データ
+df = pd.read_csv('finished_data.csv',encoding='cp932')
+
 #body
 number_N = st.sidebar.slider('N',0,60)
 number_P = st.sidebar.slider('P',0,60)
 number_K = st.sidebar.slider('K',0,60)
 
-st.number_input('Nの調整',0)
+st.data_frame(df)
+
+#st.number_input('Nの調整',0)
