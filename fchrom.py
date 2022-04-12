80f9cd8fe58cd08871bffee2a932acc971653d44
github <github>
2022-04-12 21:40:58 +0900

    dd

---
+++ fchrom.py
@@ -0,0 +1,9 @@
+import stramlit as st
+import pandas as pd
+
+#body
+number_N = st.sidebar.slider('N',0,60)
+number_P = st.sidebar.slider('P',0,60)
+number_K = st.sidebar.slider('K',0,60)
+
+st.number_input('Nの調整',0)
