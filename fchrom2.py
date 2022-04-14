1c2eda5d67b2bcd8e000c372fab3975d16f797fd
github <github>
2022-04-14 21:22:28 +0900

    ss

--- fchrom2.py
+++ fchrom2.py
@@ -73,7 +73,10 @@ if choices:
 long = len(df.index)
 st.write(f'ヒット件数:{long}')
 st.dataframe(df[["肥料の名称","肥料業者","N","P","K","肥料種類名称","登録番号"]],width=1200, height=500)
-s = st.text_input('登録番号')
+
+list_c = df['登録番号'].unique()
+s = st.multiselect('登録番号よりチラシ',list_s)
+#s = st.text_input('登録番号')
 #b = st.button('生第10000号')
 if s == '生第10000号':
     col1,col2 = st.columns(2)
