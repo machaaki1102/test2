85529623a88372af3a5ebbda9e31cc70d69d73ae
github <github>
2022-04-15 18:07:44 +0900

    ss

--- excel.py
+++ excel.py
@@ -9,8 +9,12 @@ radio = st.radio('which',select_radio)
 
 #page=0
 if radio == '新規':
-    st.text_input('得意先')
-    st.text_input('試験名')
+    tokui = st.text_input('得意先')
+    shiken = st.text_input('試験名')
+    touroku = st.button('登録')
+    if touroku:
+        with open('C:/Users/ono/Desktop/jj.txt','r') as f:
+            f.write(tokui)
 
 if radio == '追記':
     st.write('前回の続き')
