ed5c7be3be8503c8cc004597a4e5ae045ee77dd9
github <github>
2022-04-15 18:19:55 +0900

    dｄ

--- excel.py
+++ excel.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import os
 
 if 'page' not in st.session_state:
     st.session_state.page=0
@@ -15,8 +15,9 @@ if radio == '新規':
     shiken = st.text_input('試験名')
     touroku = st.button('登録')
     if touroku:
-        with open('C:/Users/ono/Desktop/jj.txt','w') as f:
+        with open('C¥Users¥ono¥Desktop¥jj.txt','w') as f:
             f.write(tokui)
+            f.close()
 
 if radio == '追記':
     st.write('前回の続き')
