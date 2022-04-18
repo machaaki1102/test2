f5f56cf6670ad6983ba6624ac9f6d1affc6a8b62
github <github>
2022-04-19 06:41:34 +0900

    ddd

--- fchrom5.py
+++ fchrom5.py
@@ -9,6 +9,7 @@ df = pd.read_csv('finished_data.csv',index_col=0,encoding='cp932')
 #sidebar
 st.sidebar.write('select')
 
+#検索機能
 with st.sidebar:
     col1, col2,col3 = st.columns(3)
     with col1:
@@ -124,6 +125,51 @@ if st.session_state.page == 0 :
     with col_k:
         number_K = st.text_input('K',0,60)
 
+#検索機能
+#with st.sidebar:
+#    col1, col2,col3 = st.columns(3)
+#    with col1:
+#        selected_N = st.checkbox('N')
+#    with col2:
+#        selected_P = st.checkbox('P')
+#    with col3:
+#        selected_K = st.checkbox('K')
+
+#number_N = st.sidebar.slider('N',0,60)
+#number_P = st.sidebar.slider('P',0,60)
+#number_K = st.sidebar.slider('K',0,60)
+    col_n1,col_p1,col_k1 = st.columns(3)
+    with col_n1:
+        puls_number_N = st.sidebar.number_input('N_adjustment',min_value=0,step =1)
+    with col_p1:
+        puls_number_P = st.sidebar.number_input('p_adjustment',min_value=0,step =1)
+    with col_k1:
+        puls_number_K = st.sidebar.number_input('K_adjustment',min_value=0,step =1)
+    #puls_number_N = st.sidebar.number_input('N_adjustment',min_value=0,step =1)
+    #puls_number_P = st.sidebar.number_input('p_adjustment',min_value=0,step =1)
+    #puls_number_K = st.sidebar.number_input('K_adjustment',min_value=0,step =1)
+
+    puls_number_N = int(puls_number_N)
+    puls_number_P = int(puls_number_P)
+    puls_number_K = int(puls_number_K)
+
+    n_amount_max = number_N + puls_number_N
+    n_amount_min = number_N - puls_number_N
+
+    p_amount_max = number_P + puls_number_P
+    p_amount_min = number_P - puls_number_P
+
+    k_amount_max = number_K + puls_number_K
+    k_amount_min = number_K - puls_number_K
+
+#        df  = df.query('@n_amount_min <= N <= @n_amount_max')
+#    if selected_P:
+#        df  = df.query('@p_amount_min <= P <= @p_amount_max')
+#    if selected_K:
+#        df  = df.query('@k_amount_min <= K <= @k_amount_max')
+
+
+
     ccsha = st.checkbox('自社品')
     tasha = st.checkbox('他社品')
     kensaku = st.button('検索')
@@ -139,7 +185,7 @@ if st.session_state.page == 0 :
    
 
           
-#  st.experimental_rerun()    
+
 #    if selected_N:
 #        df  = df.query('@n_amount_min <= N <= @n_amount_max')
 #    if selected_P:
@@ -151,10 +197,15 @@ if st.session_state.page == 0 :
 #list_s = df["肥料業者"].unique()
 choices = ['日東エフシー株式会社']
 
+df  = df.query('@n_amount_min <= N <= @n_amount_max')
+df  = df.query('@p_amount_min <= P <= @p_amount_max')
+df  = df.query('@k_amount_min <= K <= @k_amount_max')
+
+
 #if choices:
 #    df = df.query('肥料業者 == @choices')
 
-long = len(df.index)
+
 #st.write(f'ヒット件数:{long}')
 if st.session_state.page == 1:
     col_cc,col_dd = st.columns(2)
