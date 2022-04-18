1c4e3f554c4e782eb069b517254b4e70893a12ae
github <github>
2022-04-19 06:44:40 +0900

    sss

--- fchrom5.py
+++ fchrom5.py
@@ -10,35 +10,35 @@ df = pd.read_csv('finished_data.csv',index_col=0,encoding='cp932')
 st.sidebar.write('select')
 
 #検索機能
-with st.sidebar:
-    col1, col2,col3 = st.columns(3)
-    with col1:
-        selected_N = st.checkbox('N')
-    with col2:
-        selected_P = st.checkbox('P')
-    with col3:
-        selected_K = st.checkbox('K')
+#with st.sidebar:
+#    col1, col2,col3 = st.columns(3)
+#    with col1:
+#        selected_N = st.checkbox('N')
+#    with col2:
+#        selected_P = st.checkbox('P')
+#    with col3:
+#        selected_K = st.checkbox('K')
 
-number_N = st.sidebar.slider('N',0,60)
-number_P = st.sidebar.slider('P',0,60)
-number_K = st.sidebar.slider('K',0,60)
+#number_N = st.sidebar.slider('N',0,60)
+#number_P = st.sidebar.slider('P',0,60)
+#number_K = st.sidebar.slider('K',0,60)
 
-puls_number_N = st.sidebar.number_input('N_adjustment',min_value=0,step =1)
-puls_number_P = st.sidebar.number_input('p_adjustment',min_value=0,step =1)
-puls_number_K = st.sidebar.number_input('K_adjustment',min_value=0,step =1)
+#puls_number_N = st.sidebar.number_input('N_adjustment',min_value=0,step =1)
+#puls_number_P = st.sidebar.number_input('p_adjustment',min_value=0,step =1)
+#puls_number_K = st.sidebar.number_input('K_adjustment',min_value=0,step =1)
 
-puls_number_N = int(puls_number_N)
-puls_number_P = int(puls_number_P)
-puls_number_K = int(puls_number_K)
+#puls_number_N = int(puls_number_N)
+#puls_number_P = int(puls_number_P)
+#puls_number_K = int(puls_number_K)
 
-n_amount_max = number_N + puls_number_N
-n_amount_min = number_N - puls_number_N
+#n_amount_max = number_N + puls_number_N
+#n_amount_min = number_N - puls_number_N
 
-p_amount_max = number_P + puls_number_P
-p_amount_min = number_P - puls_number_P
+#p_amount_max = number_P + puls_number_P
+#p_amount_min = number_P - puls_number_P
 
-k_amount_max = number_K + puls_number_K
-k_amount_min = number_K - puls_number_K
+#k_amount_max = number_K + puls_number_K
+#k_amount_min = number_K - puls_number_K
 
 #化成＝生、輸入＝輸、なし＝原料
 #with st.sidebar:
