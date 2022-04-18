6130b6a3959c40ced6dde1758e5b9e1235d8474f
github <github>
2022-04-19 06:14:59 +0900

    dd

--- fchrom5.py
+++ fchrom5.py
@@ -137,10 +137,17 @@ if st.session_state.page == 0 :
     tasha = st.checkbox('他社品')
     kensaku = st.button('検索',on_click=ken)
 
- 
+    
+    if kensaku:
+        st.session_state.page = 1
+        if ccsha:
+            st.session_state.ccsha = 1
+
+        if tasha:
+            st.session_state.tasha = 1    
    
 
-#          
+          
 #  st.experimental_rerun()    
 #    if selected_N:
 #        df  = df.query('@n_amount_min <= N <= @n_amount_max')
